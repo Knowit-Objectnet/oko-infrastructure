b542820bb2abca639aad017d948dbb597af1b590
Tobias Kongsvik <tokongs98@gmail.com>
2020-06-25 09:21:24 +0200

    Add s3 bucket for deployment and tweak som stuff

--- calendar/staging/main.tf
+++ calendar/staging/main.tf
@@ -12,10 +12,92 @@ module "ecs_service" {
   subnets                        = data.aws_subnet_ids.private_subnets.ids
   security_groups                = [aws_security_group.ecs_service.id]
   lb_arn                         = data.aws_lb.ecs_lb.arn
-  lb_listener_port               = 80
+  lb_listener_port               = 8080
+  container_port                 = 8080
   service_discovery_namespace_id = var.service_discovery_namespace_id
   tags                           = local.tags
+  health_check_path              = "/"
+  execution_role                 = aws_iam_role.ecs_execution_role.arn
 }
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "calendar-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = ["ecs-tasks.amazonaws.com"]
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecs_execution_policy" {
+  name        = "calendar-ecs-execution-policy"
+  description = "Calendar ECS execution policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:GetParameters",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Effect = "Allow"
+        Resource = [
+          "arn:aws:ssm:*:*:parameter/calendar_db",
+          "arn:aws:logs:eu-central-1:624304543898:log-group:calendar-staging:*",
+          "arn:aws:ecr:eu-central-1:624304543898:repository/calendar"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_pull_policy" {
+  name        = "calendar-ecs-ecr-policy"
+  description = "Calendar ECS ECR pull policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Effect = "Allow"
+        Resource = [
+          "*"
+        ]
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "ecs_ecr_role_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecr_pull_policy.arn
+}
+
+
+resource "aws_iam_role_policy_attachment" "execution_role_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_policy.arn
+}
+
 
 resource "aws_security_group" "ecs_service" {
   name        = "calendar-ecs-service-staging"
@@ -24,8 +106,8 @@ resource "aws_security_group" "ecs_service" {
 
   ingress {
     protocol        = "tcp"
-    from_port       = 80
-    to_port         = 80
+    from_port       = 8080
+    to_port         = 8080
     security_groups = [data.aws_security_group.lb_sg.id]
   }
 
