e01f50c72716b5e56c57cb370a803953975f9a22
Tobias Kongsvik <tobias.kongsvik@knowit.no>
2020-07-07 13:54:55 +0200

    Add cloudfront

---
+++ frontend/production/data.tf
@@ -0,0 +1,26 @@
+data "aws_route53_zone" "oko_zone" {
+  name         = "oko.knowit.no"
+  private_zone = false
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.static.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+}
