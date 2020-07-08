e9d371c50a1960cf93495dece1a4140dc10aaca2
Jonas Carlsen <jonas.carlsen@knowit.no>
2020-07-08 18:20:33 +0200

    Added staging and prod environments for weight reporting microservice

---
+++ weight-reporting/staging/security_groups.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "weight_reporting_db" {
+  name        = "weight-reporting-db-staging"
+  description = "Security group for weight reporting database"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 5432
+    to_port         = 5432
+    security_groups = [aws_security_group.ecs_service.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ecs_service" {
+  name        = "weight-reporting-ecs-service-staging"
+  description = "Security group for weight reporting containers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
