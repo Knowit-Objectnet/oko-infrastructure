5dd04b3b001be451a87459b2ebab7036e039bb41
Tobias Kongsvik <tobias.kongsvik@knowit.no>
2020-07-16 12:54:55 +0200

    Add delete to cors and set sqs visibility timeout

--- weight-reporting/staging/sqs.tf
+++ weight-reporting/staging/sqs.tf
@@ -1,6 +1,7 @@
 resource "aws_sqs_queue" "weight-reporting_queue" {
   name                      = "weight-reporting-staging"
   receive_wait_time_seconds = 20
+  visibility_timeout_seconds = 3
 
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn
